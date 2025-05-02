3485da2c519336c666d9360e8f92089773b21ed3
paulinapglz99 <paulinapglz.99@gmail.com>
2025-05-02 21:25:00 +0000

    already demuxed matrices

--- 1.demultiplex_matrices.R
+++ 1.demultiplex_matrices.R
@@ -1,29 +1,18 @@
 #!/usr/bin/env Rscript
 
-#Script to call for cell matrices and metadata for
-#Demultiplexing count matrices per individual
-
 start.time <- Sys.time()
 
-#Libraries --- ---
+# Libraries
 message("#Libraries --- ---")
+pacman::p_load("tidyverse", "Seurat", "SingleCellExperiment", "furrr", "vroom", "optparse")
 
-#install.packages('Seurat')
-
-pacman::p_load("tidyverse", 
-               "Seurat", 
-               "ggplot2", 
-               "SingleCellExperiment", 
-               "furrr", 
-               "vroom", 
-               "optparse")
-
-#Options from console --- ---
-
+# Command-line options
 option_list <- list(
   make_option(c("-d", "--directory"), type = "character", help = "Directory with MTX matrices", metavar = "character"),
   make_option(c("-m", "--metadata"), type = "character", help = "File with demultiplexing data", metavar = "character"),
-  make_option(c("-o", "--output"), type = "character", help = "Output file .rds", metavar = "character")
+  make_option(c("-o", "--output"), type = "character", help = "Output file .rds", metavar = "character"),
+  make_option(c("-t", "--test"), action = "store_true", default = FALSE,
+              help = "Run test mode (only one batch)")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -34,109 +23,72 @@ if (is.null(opt$directory) | is.null(opt$metadata) | is.null(opt$output)) {
   stop("Mandatory arguments are missing: --directory, --metadata and/or --output", call. = FALSE)
 }
 
-#Load functions --- ---
-message("#Load functions --- ---")
-
-#Parallel implementation plan for future
+# Parallel setup
+message("#Parallel setup --- ---")
 set.seed(10)
-future::plan(multisession, workers = 5)
+future::plan(multisession, workers = 10)
 
-#Function to read matrix
+message("#Set functions --- ---")
 
-read_matrix.f <- function(sample) {
-  message("Reading sample: ", sample)
+# Demultiplexing function
+demultiplex.f <- function(batch_name) {
+  message("Reading and demultiplexing batch: ", batch_name)
   
   counts <- ReadMtx(
-    mtx = file.path(directory, paste0(sample, ".matrix.mtx.gz")),
-    features = file.path(directory, paste0(sample, ".features.tsv.gz")),
-    cells = file.path(directory, paste0(sample, ".barcodes.tsv.gz"))
+    mtx = file.path(opt$directory, paste0(batch_name, ".matrix.mtx.gz")),
+    features = file.path(opt$directory, paste0(batch_name, ".features.tsv.gz")),
+    cells = file.path(opt$directory, paste0(batch_name, ".barcodes.tsv.gz"))
   )
   
-  seurat_obj <- CreateSeuratObject(counts = counts, project = sample)
-  seurat_obj$sample <- sample
-  return(seurat_obj)
-}
-
-#Function to demultiplex matrix
-
-demultiplex.f <- function(batch_name) {
-  #Filter only corresponding batch
-  demux_subset <- demultiplex.df %>% filter(libraryBatch == batch_name)
-  #Cell barcodes of the batch
-  barcodes <- names(seurat_list[[batch_name]]$sample)
-  #Match demultiplex data with barcodes
-  meta <- data.frame(cell = barcodes) %>%
+  seurat_obj <- CreateSeuratObject(counts = counts, project = batch_name)
+  seurat_obj$sample <- batch_name
+  
+  demux_subset <- demultiplex.df %>%
+    filter(libraryBatch == batch_name)
+  
+  meta <- tibble(cell = colnames(seurat_obj)) %>%
     left_join(demux_subset, by = c("cell" = "cellBarcode")) %>%
-    filter(!is.na(individualID)) %>% 
+    filter(!is.na(individualID)) %>%
     column_to_rownames("cell")
-  #Assign metadata to the Seurat object
-  seurat_obj <- AddMetaData(seurat_list[[batch_name]], metadata = meta)
+  
+  seurat_obj <- AddMetaData(seurat_obj, metadata = meta)
   seurat_ind <- SplitObject(seurat_obj, split.by = "individualID")
-  #Rename element in format batch_individualID
   names(seurat_ind) <- paste0(batch_name, "_", names(seurat_ind))
   
   message(Sys.time(), " - Processed individuals: ", length(seurat_ind))
   return(seurat_ind)
-  
 }
 
-#Get data --- ---
-message("#Get data --- ---")
+# Load demultiplex metadata
+message("#Loading metadata --- ---")
+demultiplex.df <- vroom(opt$metadata) %>% as_tibble()
 
-#Set directory
-#directory <- "/datos/rosmap/single_cell/matrix_exp_2-minimal/"
-directory <- opt$directory
-
-#Obtain matrix files .mtx.gz
-
-matrix_files <- list.files(directory,
-                           pattern = "\\.matrix\\.mtx\\.gz$", full.names = TRUE)
-
-#Obtain batch names
+# Get batch names
+matrix_files <- list.files(opt$directory, pattern = "\\.matrix\\.mtx\\.gz$", full.names = TRUE)
 batch_names <- str_replace(basename(matrix_files), "\\.matrix\\.mtx\\.gz$", "")
+if (opt$test) {
+  message("# Test mode active: running on one batch only --- ---")
+  batch_names <- batch_names[1]
+}
 
-#List of Seurat objects
-seurat_list <- future_map(batch_names, read_matrix.f)
-names(seurat_list) <- batch_names
-
-#Demultiplex --- ---
-
-message("#Demultiplex --- ---")
-
-#Try this function 
-# 
-# seurat_obj <- seurat_list[1]
-# batch_name <- batch_names[1]
-# 
-#test <- demultiplex.f(batch_name = batch_names[1])
-
-demultiplex.df <- vroom(opt$metadata)
-#demultiplex.df <- vroom(file = "/datos/rosmap/single_cell/metadata/ROSMAP_snRNAseq_demultiplexed_ID_mapping.csv")
-
-#Parallel version using future_map
-
+# Run demultiplexing
+message("#Running demultiplexing in parallel --- ---")
 list_seurat_ind <- future_map(
-  batch_names, 
+  batch_names,
   demultiplex.f,
   .options = furrr_options(
-    #scheduling = 2,
-    seed = TRUE,           # Para reproducibilidad
-    globals = c("seurat_list", "demultiplex.df"),  # Exportar variables a los workers
-    packages = c("dplyr", "Seurat", "tibble")  # Paquetes necesarios en workers
+    seed = TRUE,
+    globals = c("demultiplex.df","opt"),
+    packages = c("dplyr", "Seurat", "tibble")
   ),
-  .progress = TRUE        # Barra de progreso
+  .progress = TRUE
 )
 names(list_seurat_ind) <- batch_names
 
-#Flatten
-message("#Flatten demultiplex --- ---")
-
+# Flatten and save
+message("#Flattening and saving --- ---")
 list_seurat_ind <- flatten(list_seurat_ind)
+saveRDS(list_seurat_ind, file = opt$output,  compress = "gzip")
 
-#Save list
-
-#saveRDS(list_seurat_ind, file = "/datos/rosmap/single_cell/matrices_demultiplexed.rds")
-saveRDS(list_seurat_ind, file = opt$output)
-
-message("Running time:", Sys.time() - start.time, " s")
+message("Running time:", Sys.time() - start.time, " min")
 #END
--- run_demultiplex.sh
+++ run_demultiplex.sh
@@ -1,4 +1,5 @@
-Rscript 1.demultiplex.R \
-  --directory /datos/rosmap/single_cell/matrix_exp_2-minimal/ \
+Rscript ~/single_cell_networks/CopyOf1.demultiplex_matrices.R \
+  --directory /datos/rosmap/single_cell/matrix_exp_2/ \
   --metadata /datos/rosmap/single_cell/metadata/ROSMAP_snRNAseq_demultiplexed_ID_mapping.csv \
-  --output /datos/rosmap/single_cell/matrices_demultiplexed.rds
+  --output /datos/rosmap/single_cell/full_demultiplexed_matrices/matrices_demultiplexed_proof.rds
+  --test
