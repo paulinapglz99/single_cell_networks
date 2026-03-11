ab3f8349aca4523889d23f09a2f54d8a8ebf54a0
Danae Alejandra Cortes García <alejandradanae14@gmail.com>
2026-03-10 18:29:26 -0600

    Update run_3.merge_integration.sh

--- run_3.merge_integration.sh
+++ run_3.merge_integration.sh
@@ -1,6 +1,6 @@
 Rscript ~/single_cell_networks/3.merge_integration.R \
 -s /STORAGE/csbig/sc_ADers/matrices_demultiplexed_final_QC-2026-01-28_19-55/seurat_list_filtered.rds \
 -a /STORAGE/csbig/sc_ADers/metadata/ROSMAP_assay_scrnaSeq_metadata.csv \
--c /STORAGE/csbig/sc_ADers/metadata/tables/clinical_stratified.csv \
--o /STORAGE/csbig/sc_ADers/merge_integration_results_feb10 \
+-c /STORAGE/csbig/sc_ADers/metadata/tables/clinical_stratified_s4.csv \
+-o /STORAGE/csbig/sc_ADers/merge_integration_results_mar10_s4 \
 -w 12
