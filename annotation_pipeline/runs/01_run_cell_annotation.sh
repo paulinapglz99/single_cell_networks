53766f79f455c23349dbd033ca19e5415c04efd1
Dacortes <alejandradanae14gmail.com>
2026-05-08 10:31:41 -0600

    fix the run paths

--- annotation_pipeline/runs/01_run_cell_annotation.sh
+++ annotation_pipeline/runs/01_run_cell_annotation.sh
@@ -1,6 +1,6 @@
-Rscript ~/Single_cell_pre/cell_annotation.R \
-  --seurat_rds /STORAGE/csbig/sc_ADers/merge_integration_results_feb10/merged_by_individual_harmony.rds \
+Rscript ~/single_cell_networks/01_cell_annotation.R \
+  --seurat_rds /STORAGE/csbig/sc_ADers/merge_integration_results_mar10_s4/merged_by_individual_harmony.rds \
   --anno_csv /STORAGE/csbig/sc_ADers/metadata/Experiment2/cell-annotation.full-atlas.csv \
-  --out_dir /STORAGE/csbig/sc_ADers/out_annotated_final \
+  --out_dir /STORAGE/csbig/sc_ADers/out_annotated_final_s4 \
   -w 12 \
   --seed 42
--- annotation_pipeline/runs/03_run_celltypist.sh
+++ annotation_pipeline/runs/03_run_celltypist.sh
@@ -1,7 +1,7 @@
 
-python ~/Single_cell_pre/Celltypi_anno.py \
-  --base /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final \
+python ~/single_cell_networks/03_Celltypist_anno.py \
+  --base /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final_s4 \
   --model /STORAGE/csbig/sc_ADers/celltipy_model_PC.pkl \
   --majority_voting \
   --min_prob 0.6 \
-  --out_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final/unassigned_celltypist_predictions.csv
+  --out_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final_s4/unassigned_celltypist_predictions_s4.csv
