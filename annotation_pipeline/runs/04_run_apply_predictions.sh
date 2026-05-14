c5e9b18fedd09c2ab13812525c1a2a3650120d9e
Dacortes <alejandradanae14gmail.com>
2026-05-14 16:44:49 -0600

    correcciones menores de runs

--- annotation_pipeline/runs/04_run_apply_predictions.sh
+++ annotation_pipeline/runs/04_run_apply_predictions.sh
@@ -1,6 +1,6 @@
 Rscript ~/single_cell_networks/04_apply_predictions_CT.R\
    --seurat_rds /STORAGE/csbig/sc_ADers/out_annotated_final_s4/merged_harmony_integrated_annotated.rds \
    --pred_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final_s4/unassigned_celltypist_predictions_s4.csv \
-   --out_dir /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final/annotation_with_celltypist_s4 \
+   --out_dir /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final_s4/annotation_with_celltypist_s4 \
    -w 12 \
    --seed 42
